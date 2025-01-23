17771bb065acaa4b5e3bcfc8cd574534108f634b
sagar-lonkar <sagar.lonkar@domo.com>
2025-01-23 11:56:12 +0530

    java 8 test

---
+++ src/main/java/com/persistent/test/Address.java
@@ -0,0 +1,16 @@
+package com.persistent.test;
+
+public class Address {
+
+	private String city;
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+}
